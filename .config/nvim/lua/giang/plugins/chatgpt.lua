ed2a8c6bb2853af56a97139308fff48c48abaca6
Giang <nguyentonggiang131294@gmail.com>
2024-06-05 21:13:46 +0700

    Update nvim plugins

---
+++ .config/nvim/lua/giang/plugins/chatgpt.lua
@@ -0,0 +1,10 @@
+return {
+  "jackMort/ChatGPT.nvim",
+  event = "VeryLazy",
+  dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+  config = function()
+    require("chatgpt").setup({
+      api_key_cmd = "op read op://Personal/OpenAI/credential --no-newline",
+    })
+  end,
+}
