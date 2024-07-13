dc1e113a1ca8ad63d28658d8a3f2e1e1934f2772
Giang <nguyentonggiang131294@gmail.com>
2024-07-13 10:24:45 +0700

    add some keymaps

--- .config/nvim/lua/giang/plugins/elixir-tool.lua
+++ .config/nvim/lua/giang/plugins/elixir-tool.lua
@@ -16,9 +16,9 @@ return {
           enableTestLenses = false,
         }),
         on_attach = function(client, bufnr)
-          vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
-          vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
-          vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
+          vim.keymap.set("n", "<space>efp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
+          vim.keymap.set("n", "<space>etp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
+          vim.keymap.set("v", "<space>eem", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
         end,
       },
     })
